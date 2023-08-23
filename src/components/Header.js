29688b366225c4cf4c2b47830546c5e2979e0b12
= <shivamsingh201617@gmail.com>
2023-08-24 04:06:23 +0530

    signup

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <a href="#home" className={styles.navLink}>
+          Home
+        </a>
+        <a href="#product" className={styles.navLink}>
+          Product
+        </a>
+        <a href="#about" className={styles.navLink}>
+          About Us
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Pages/SignUp.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+const Signup = ({ onSubmit }) => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmpassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ username, email, password,confirmpassword });
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmpassword}
+          onChange={(e) => setConfirmpassword(e.target.value)}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
